baeb6c2e8f097715c7ba2f19b9c11cdd4e8b042a
Ivan LOISY <ivloisy@e3r9p2.clusters.42paris.fr>
2022-05-03 13:11:29 +0200

    enable_if

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -4,88 +4,117 @@
 namespace ft
 {
 
-	template< typename T > 
+	/*
+	**	is_integral
+	*/
+
+	template< class T >
 	struct is_integral
 	{
 		static const bool	value = false;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<bool>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<char>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	// template<>
+	// struct is_integral<char16_t>
+	// {
+	// 	static const bool value = true;
+	// };
+
+	// template<>
+	// struct is_integral<char32_t>
+	// {
+	// 	static const bool value = true;
+	// }
+
+	template<>
 	struct is_integral<wchar_t>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<signed char>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<short int>
-	{ 
+	{
 		static const bool value = true; 
 	};
 
-	template<> 
+	template<>
 	struct is_integral<int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<long int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<long long int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<unsigned char>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<unsigned short int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<unsigned int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<unsigned long int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
 	};
 
-	template<> 
+	template<>
 	struct is_integral<unsigned long long int>
-	{ 
-		static const bool value = true; 
+	{
+		static const bool value = true;
+	};
+
+	/*
+	**	enable_if
+	*/
+
+	template <bool Cond, class T = void>
+	struct enable_if {};
+
+	template <class T>
+	struct enable_if<true, T>
+	{
+		typedef T	type;
 	};
 
 }
