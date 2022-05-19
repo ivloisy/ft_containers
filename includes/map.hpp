f7cf02c32aa583ad2dc2795e9c2e03e21322485b
ivloisy <ivloisy@student.42.fr>
2022-05-19 02:38:47 +0200

    test

--- includes/map.hpp
+++ includes/map.hpp
@@ -8,6 +8,8 @@
 # include "type_traits.hpp"
 # include "utility.hpp"
 
+
+
 namespace ft
 {
 	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
